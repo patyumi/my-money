b5dce156af2e0731f60659ed3d0a72429010f168
Patrícia Yumi <pati.yumi@yahoo.com.br>
2019-08-18 10:45:01 -0300

    configurações inicias

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useReducer } from 'react';
+import axios from 'axios';
+
+const url = 'https://mymoney-curso-devpleno.firebaseio.com/movimentacoes/2019-08.json';
+
+// Função pura (mais fácil de testar)
+const reducer = (state, action) => {
+  if (action.type === 'REQUEST') {
+    return {
+      ...state,
+      loading: true,
+    };
+  }
+  if (action.type === 'SUCCESS') {
+    return {
+      ...state,
+      loading: false,
+      data: action.data,
+    };
+  }
+
+  return state;
+};
 
 function App() {
+  // HOOKS: useReduer
+  const [data, dispatch] = useReducer(reducer, {
+    loading: true,
+    data: {},
+  });
+
+  // HOOKS: useEffect
+  useEffect(() => {
+    dispatch({ type: 'REQUEST' });
+    axios
+      .get(url)
+      .then((res) => {
+        dispatch({ type: 'SUCCESS', data: res.data });
+      });
+  }, []);
+
+  // Elementos
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Olá</h1>
+      { JSON.stringify(data) }
+      { data.loading && <p>loading...</p> }
     </div>
   );
 }
